850e1efc4631b1e9e551b39662ed8dc905917d8c
Sangwonee <sangw12477@gmail.com>
2024-08-13 00:44:46 +0900

    fix : friend 목록조회시 응답값으로 nickname추가

--- src/main/java/com/pj/planjourney/domain/friend/dto/FriendResponseDto.java
+++ src/main/java/com/pj/planjourney/domain/friend/dto/FriendResponseDto.java
@@ -9,9 +9,13 @@ import lombok.Setter;
 public class FriendResponseDto {
     private Long userId;
     private Long friendId;
+    private String friendNickname;
+    private String userNickname;
 
     public FriendResponseDto(Friend friend) {
         this.userId = friend.getUser().getId();
         this.friendId = friend.getFriend().getId();
+        this.friendNickname = friend.getFriend().getNickname();
+        this.userNickname = friend.getUser().getNickname();
     }
 }
