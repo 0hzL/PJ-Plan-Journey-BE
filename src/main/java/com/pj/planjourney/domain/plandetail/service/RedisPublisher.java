e5e83ecd7a173b7116d0244d7afd6b3b37319467
Davin <85967571+dav1n9@users.noreply.github.com>
2024-08-14 19:45:58 +0900

    feature : log 추가

--- src/main/java/com/pj/planjourney/domain/plandetail/service/RedisPublisher.java
+++ src/main/java/com/pj/planjourney/domain/plandetail/service/RedisPublisher.java
@@ -3,9 +3,11 @@ package com.pj.planjourney.domain.plandetail.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pj.planjourney.domain.plandetail.dto.EditPlanDetailResponseDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RedisPublisher {
@@ -16,6 +18,7 @@ public class RedisPublisher {
     public void publish(EditPlanDetailResponseDto message) {
         try {
             String response = objectMapper.writeValueAsString(message);
+            log.info("response : " + response);
             pubSubRedisTemplate.convertAndSend("planUpdates", response);
         } catch (Exception e) {
             e.printStackTrace();
